70a12119d4908fdb60188b808b766df74086192d
0215J <153706779+0215J@users.noreply.github.com>
2024-11-29 22:43:36 +0800

    Update url.js

--- url.js
+++ url.js
@@ -11,5 +11,5 @@ const urls = [
 	'https://www.surveycake.com/s/og79m',
 	'https://www.surveycake.com/s/egev',
 	'https://www.surveycake.com/s/YgMOo',
-	'https://www.surveycake.com/s/YgMOo'
+	'https://www.surveycake.com/s/Nqy3e'
 ];
