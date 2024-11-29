32e521e5485d7ebbf09bf1640669299c87f32473
0215J <153706779+0215J@users.noreply.github.com>
2024-11-29 22:33:30 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,8 +8,8 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLSej-NBbMz_jtmf2i8vUE_ywHwFr5yluwSr5dDvKhJX8u_ORaQ/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSdX-J7Qt8NINbWxpEw_VRpJzU3xPVDBRnd9b4yo7ijjNbxZ-w/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLScAhVVzLQcqWmbIccGe-uXA0R8QF5_wdi04VxtdehzX9FOhvw/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLScdZHUQfzDGz7QDsAA2I2-rKinNip6ICneZpqRsk5j4JYFPJA/viewform'
+	'https://www.surveycake.com/s/og79m',
+	'https://www.surveycake.com/s/egev',
+	'https://www.surveycake.com/s/YgMOo',
+	'https://www.surveycake.com/s/YgMOo'
 ];
